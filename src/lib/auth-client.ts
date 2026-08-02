d892f6a078383eb494c40ecb507b4e4224d064b2
ozaharsh95 <dsouzaaron77@gmail.com>
2026-08-02 18:57:58 +0530

    fix:Enabled Cross-Site Cookies for Production Authentication

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,6 +3,9 @@ import { BACKEND_BASE_URL, USER_ROLES } from "@/constants";
 
 export const authClient = createAuthClient({
   baseURL: `${BACKEND_BASE_URL}/auth`, // The base URL of your auth server -> example -> BACKEND_BASE_URL="http://localhost:8000/api/"
+  fetchOptions: {
+    credentials: "include",
+  },
 
   user: {
     additionalFields: {
